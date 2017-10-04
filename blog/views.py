d5b1f64c5f41fc2f0497004b368444f56980a093
Aula S2B <luciano.fernando@gmail.com>
2017-10-04 20:57:58 -0300

    update

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def contato(request):
+    return render(request, 'contato.html')
