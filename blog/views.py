e07aed1bec015ee13e33367bde827e6d7b66dd7d
Aula S2B <luciano.fernando@gmail.com>
2017-10-04 21:56:27 -0300

    update

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'index.html', {'posts': posts})
 
-def contato(request):
-    return render(request, 'contato.html')
